8963780191206abd1556968eb33c2a42a896a9f1
glenholmes <me@glenholmes.me>
2020-11-12 14:32:57 +0000

    FEAT: Employee Dialog to replace View - Added Fragment - Changed event to load Dialog on list item click

--- webapp/controller/EmployeeList.controller.js
+++ webapp/controller/EmployeeList.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+	"sap/ui/demo/nav/controller/BaseController",
+	"sap/ui/core/Fragment"
+], function (BaseController, Fragment) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.core.mvc.controller.EmployeeList", {
@@ -13,8 +14,23 @@ sap.ui.define([
 				employeeId: oCtx.getProperty("EmployeeID")
 			});
 
-		}
+		},
 
+		onListItemPressedDialog: function (oEvent) {
+			var oView = this.getView();
+
+			if (!this.byId("EmployeeDialog")) {
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.nav.view.dialogs.EmployeeDialog"
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("EmployeeDialog").open();
+			}
+		}
 	});
 
 });
